1301aa2a392734c4d69b6cc6c1f27ad9a1194dfe
imtiyaz <imtiyazali6692@gmail.com>
2024-08-12 11:32:00 +0530

    react JS program

---
+++ 01react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai or react | Imtiyaz</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1>Chai or react with vite| imtiyaz</h1>
+  )
+}
+
+export default App
